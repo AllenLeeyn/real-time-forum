c5b042a326ad9306b138cbf241e99cafe6aa7536
kurizma <foriamkuri@gmail.com>
2025-03-25 00:21:19 +0200

    Feed - No Longer Table; dynamic generated semantic HTML containers

--- assets/static/script/feed.js
+++ assets/static/script/feed.js
@@ -1,23 +1,44 @@
 import { insertPostCard } from "./post.js";
 
-export function getFeed(posts){
-  const container = document.createElement('tbody');
+export function getFeed(posts) {
+  console.log('Creating feed for:', posts);
+  const container = document.getElementById('feedDisplay');
 
-  if (!Array.isArray(posts) || posts.length === 0){
+  if (!Array.isArray(posts) || posts.length === 0) {
     container.innerHTML = `
-    <tbody><tr><td>
-      <div class="post-card">
-        <h3>
-          No post found
-        </h3>
-      </div>
-    </tbody></tr></td>`;
+      <article>
+        <h3>No post found</h3>
+      </article>
+    `;
   } else {
-    posts.forEach(post =>{
+    container.innerHTML = ''; 
+    posts.forEach(post => {
+      console.log('Creating post card for:', post);
       insertPostCard(post, container);
+      console.log('Post card created and appended.');
     });
   }
-
-  return container;
+  return container.innerHTML;
 }
+
+// export function getFeed(posts){
+//   const container = document.createElement('tbody');
+
+//   if (!Array.isArray(posts) || posts.length === 0){
+//     container.innerHTML = `
+//     <tbody><tr><td>
+//       <div class="post-card">
+//         <h3>
+//           No post found
+//         </h3>
+//       </div>
+//     </tbody></tr></td>`;
+//   } else {
+//     posts.forEach(post =>{
+//       insertPostCard(post, container);
+//     });
+//   }
+
+//   return container;
+// }
   
